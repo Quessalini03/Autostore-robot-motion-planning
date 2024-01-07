e9db5520cdbaa1cd4a01ec940abe9c4e3b77deae
Quessalini03 <vuchauduyquang@gmail.com>
2024-01-07 22:36:31 +0700

    eval: change params to run evals

--- evaluate.py
+++ evaluate.py
@@ -9,9 +9,9 @@ from torch.utils.tensorboard import SummaryWriter
 
 
 class args:
-    num_columns = 13
-    num_rows = 13
-    num_agents = 10
+    num_columns = 18
+    num_rows = 18
+    num_agents = 6
 
     num_scenarios = 1000
 
@@ -30,11 +30,16 @@ class Evaluator(BaseEvaluator):
         self.writer_DQN = SummaryWriter(self.get_eval_directory() + '/DQN')
         self.writer_DDQN = SummaryWriter(self.get_eval_directory() + '/DDQN')
 
+        self.save_evaluate_params(args)
+
     def evaluate(self):
         world = World(args.num_columns, args.num_rows, args.num_agents)
         world.eval()
         total_reward = 0.0
         progess_bar = tqdm(total=args.num_scenarios, desc="Scenarios completed", leave=True, unit_scale=True, unit='Scenario', colour='green')
+
+        arrived_DQN = 0
+        arrived_DDQN = 0
 
         for scenario in range(args.num_scenarios):
             # DQN
@@ -50,10 +55,11 @@ class Evaluator(BaseEvaluator):
                     state = agent.get_state()
                     action = agent.get_action(self.policy_net_DQN, state)
                     # print(f"Agent {agent_idx} is moving in epoch {epoch} with action {action}")
-                    reward, done = agent.perform_action(action)
+                    reward, done, arrived = agent.perform_action(action)
 
-                    if done and reward > 0:
+                    if arrived:
                         arrived_at_goal += 1
+                        arrived_DQN += 1
                     #     print(f"Agent {agent_idx} has reached the goal in epoch {epoch} with reward {reward}!")
 
                     total_reward += reward
@@ -88,10 +94,11 @@ class Evaluator(BaseEvaluator):
                     state = agent.get_state()
                     action = agent.get_action(self.policy_net_DDQN, state)
                     # print(f"Agent {agent_idx} is moving in epoch {epoch} with action {action}")
-                    reward, done = agent.perform_action(action)
+                    reward, done, arrived = agent.perform_action(action)
 
-                    if done and reward > 0:
+                    if arrived:
                         arrived_at_goal += 1
+                        arrived_DDQN += 1
                     #     print(f"Agent {agent_idx} has reached the goal in epoch {epoch} with reward {reward}!")
 
                     total_reward += reward
@@ -111,6 +118,8 @@ class Evaluator(BaseEvaluator):
 
             progess_bar.update(1)
 
+        self.writer_DQN.add_scalar('Arrival rate/total', arrived_DQN/(args.num_scenarios * args.num_agents), 0)
+        self.writer_DDQN.add_scalar('Arrival rate/total', arrived_DDQN/(args.num_scenarios * args.num_agents), 0)
 
         self.writer_DDQN.close()
         self.writer_DQN.close()
