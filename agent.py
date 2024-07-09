c4fe208cdefa7bd0f3c7267af606976a6cd9b0fd
leanhhuy0709 <huy.leanh0709@hcmut.edu.vn>
2024-07-09 22:18:57 +0700

    update visualize

--- agent.py
+++ agent.py
@@ -10,6 +10,7 @@ import torch.nn as nn
 from config import args, Action
 from models.QNet import QNetwork, QLoss, ReplayMemory, Experience
 
+
 class World:
     def __init__(self, num_columns, num_rows, num_bots) -> None:
         self.num_columns = num_columns
@@ -28,7 +29,6 @@ class World:
         self.current_positions_copy = []
         self.goal_positions_copy = []
 
-
         self.num_actions_performed = [0 for _ in range(self.num_bots)]
         self._init_positions()
         self.frequent = []
@@ -41,7 +41,7 @@ class World:
         for i in range(1, self.num_rows - 1):
             self.wall_positions.append((0, i))
             self.wall_positions.append((self.num_columns - 1, i))
-        
+
         for i in range(self.num_columns):
             self.wall_positions.append((i, 0))
             self.wall_positions.append((i, self.num_rows - 1))
@@ -73,7 +73,7 @@ class World:
         self.wall_positions_copy = deepcopy(self.wall_positions)
         self.start_postitions_copy = deepcopy(self.start_postitions)
         self.current_positions_copy = deepcopy(self.current_positions)
-        self.goal_positions_copy = deepcopy(self.goal_positions)   
+        self.goal_positions_copy = deepcopy(self.goal_positions)
 
     def eval(self):
         for i in range(self.num_bots):
@@ -95,20 +95,20 @@ class World:
         row = np.random.randint(0, self.num_rows)
 
         return (column, row)
-    
+
     def is_valid_position(self, position, column_offset, row_offset):
         current_col, current_row = position
         position = (current_col + column_offset, current_row + row_offset)
 
         if position in self.wall_positions:
             return False
-        
+
         if position[0] >= self.num_columns or \
-            position[1] >= self.num_rows or \
-            position[0] < 0 or \
-            position[1] < 0:
-                return False
-        
+                position[1] >= self.num_rows or \
+                position[0] < 0 or \
+                position[1] < 0:
+            return False
+
         return True
 
     def current_position_is_of(self, position: Tuple[int, int]):
@@ -117,18 +117,18 @@ class World:
                 continue
             if self.current_positions[i] == position:
                 return i
-        
+
         return -1
-    
+
     def goal_position_of(self, position: Tuple[int, int]):
         for i in range(self.num_bots):
             if self.agent_lists[i].is_alive == False:
                 continue
             if self.goal_positions[i] == position:
                 return i
-        
+
         return -1
-    
+
     def _move(self, action: int, agent_index: int):
         cur_col, cur_row = self.current_positions[agent_index]
         if action == Action.UP:
@@ -139,7 +139,7 @@ class World:
             cur_col -= 1
         elif action == Action.RIGHT:
             cur_col += 1
-        
+
         return (cur_col, cur_row)
 
     def _move_inplace(self, action: int, agent_index: int):
@@ -152,7 +152,7 @@ class World:
         current_position = self.current_positions[agent_index]
         if current_position in self.wall_positions:
             return True
-        
+
         # collide with other living agents
         for i in range(self.num_bots):
             if i == agent_index:
@@ -161,13 +161,13 @@ class World:
                 continue
             if self.current_positions[i] == current_position:
                 return True
-            
+
         return False
 
     def l2_distance(self, position1, position2):
         return np.sqrt((position1[0] - position2[0])**2 + (position1[1] - position2[1])**2)
 
-    def perform_action(self, action: int, agent_index: int):
+    def perform_action(self, action: int, agent_index: int, step_value=1):
         # self.frequent[self.current_positions[agent_index][0]][self.current_positions[agent_index][1]] += 1.0
         prev_position = self.current_positions[agent_index]
         self._move_inplace(action, agent_index)
@@ -180,7 +180,7 @@ class World:
             reward = -2
             self.agent_lists[agent_index].is_alive = False
             return reward, done, arrived
-        
+
         agent = self.agent_lists[agent_index]
         agent.time_to_live -= 1
         if agent.time_to_live <= 0:
@@ -188,17 +188,19 @@ class World:
             reward = -0.01
             self.agent_lists[agent_index].is_alive = False
             return reward, done, arrived
-        
+
         if self.current_positions[agent_index] == self.goal_positions[agent_index]:
             done = 1
             reward = 2
             arrived = True
             # self.agent_lists[agent_index].is_alive = False
             return reward, done, arrived
-        
-        prev_goal_distance = self.l2_distance(prev_position, self.goal_positions[agent_index])
-        curr_goal_distance = self.l2_distance(self.current_positions[agent_index], self.goal_positions[agent_index])
-        
+
+        prev_goal_distance = self.l2_distance(
+            prev_position, self.goal_positions[agent_index])
+        curr_goal_distance = self.l2_distance(
+            self.current_positions[agent_index], self.goal_positions[agent_index])
+
         reward = self.omega * (prev_goal_distance - curr_goal_distance)
         done = 0
         return reward, done, arrived
@@ -212,7 +214,7 @@ class World:
            curr_column < 0 or \
            curr_row < 0:
             return False
-        
+
         for i in range(self.num_bots):
             if i == agent_index:
                 continue
@@ -220,11 +222,8 @@ class World:
                 continue
             if self.current_positions[i] == current_position:
                 return False
-            
+
         return True
-
-    
-
 
 
 class Agent:
@@ -232,15 +231,16 @@ class Agent:
     epsilon_decrement = args.epsilon_decrement
     gamma = args.gamma
 
-    obstacle_weight_vector = torch.tensor([0.2, 0.4, 0.6, 0.8, 1.0], dtype=torch.float32)
+    obstacle_weight_vector = torch.tensor(
+        [0.2, 0.4, 0.6, 0.8, 1.0], dtype=torch.float32)
 
     def __init__(
         self,
         world: World,
         index_in_world: int,
-        time_to_live = 150,
-        num_actions = 5,
-        eval = False
+        time_to_live=150,
+        num_actions=5,
+        eval=False
     ):
         self.world = world
         self.index_in_world = index_in_world
@@ -252,7 +252,8 @@ class Agent:
         self.previous_move = 0
 
         self.default_state = [[0, 0, 0, 0, 0] for _ in range(5)]
-        self.observation_history = deque([self.default_state for _ in range(3)], maxlen=3)
+        self.observation_history = deque(
+            [self.default_state for _ in range(3)], maxlen=3)
         self.is_first_observation = True
 
         # heuristic
@@ -281,30 +282,32 @@ class Agent:
     def calculate_temporary_goal(self):
         on_goal_row_axis = random.choice([True, False])
         if on_goal_row_axis:
-            self.temporary_goal = (self.world.current_positions[self.index_in_world][0], self.world.goal_positions[self.index_in_world][1])
+            self.temporary_goal = (
+                self.world.current_positions[self.index_in_world][0], self.world.goal_positions[self.index_in_world][1])
         else:
-            self.temporary_goal = (self.world.goal_positions[self.index_in_world][0], self.world.current_positions[self.index_in_world][1])
+            self.temporary_goal = (
+                self.world.goal_positions[self.index_in_world][0], self.world.current_positions[self.index_in_world][1])
 
     def has_obstacles(self, state):
         surrounding = state["observation"][2]
         return torch.sum(surrounding) > 0
-    
+
     def see_obstacle(self):
         self.seen_obstacles.append(1)
 
     def should_move_heuristic(self, state):
         if self.has_obstacles(state):
             return False
-        
+
         if self.temporary_goal == None:
             self.calculate_temporary_goal()
             return True
         if self.world.current_positions[self.index_in_world] == self.temporary_goal:
             self.temporary_goal = self.world.goal_positions[self.index_in_world]
             return True
-        
+
         return True
-    
+
     def get_heuristic_action(self):
         current_position = self.world.current_positions[self.index_in_world]
         goal_position = self.temporary_goal
@@ -323,18 +326,20 @@ class Agent:
             else:
                 return Action.LEFT
         else:
-            raise Exception("Invalid temporary goal " + str(self.temporary_goal) + " with position " + str(current_position))
-        
+            raise Exception("Invalid temporary goal " + str(self.temporary_goal) +
+                            " with position " + str(current_position))
+
     def should_force_policy(self):
         return self.remaining_policy_steps > 0
-    
+
     def post_force_policy(self):
         if self.remaining_policy_steps > 0:
             self.remaining_policy_steps -= 1
             if self.remaining_policy_steps == 0:
                 self.calculate_temporary_goal()
         else:
-            raise Exception("Remaining policy steps is not greater than 0 after force policy")
+            raise Exception(
+                "Remaining policy steps is not greater than 0 after force policy")
 
     def post_normal_policy(self):
         self.temporary_goal = None
@@ -345,7 +350,8 @@ class Agent:
     def reset(self):
         self.time_to_live = 150
         self.is_alive = True
-        self.observation_history = deque([self.default_state for _ in range(3)], maxlen=3)
+        self.observation_history = deque(
+            [self.default_state for _ in range(3)], maxlen=3)
         self.is_first_observation = True
         self.seen_obstacles = deque(self.default_seen_obstacles, maxlen=5)
         self.remaining_policy_steps = 0
@@ -356,7 +362,7 @@ class Agent:
     def get_state(self):
         current_position = self.world.current_positions[self.index_in_world]
         goal_position = self.world.goal_positions[self.index_in_world]
-        
+
         current_col, current_row = current_position
         # check surrounding regions: [valid_position, other_agent_present, prev_move_up, prev_move_down, prev_move_left, prev_move_right, prev_move_static] * 8
         # 5*5 center
@@ -367,27 +373,31 @@ class Agent:
                 if row_offset == 0 and col_offset == 0:
                     continue
                 if not self.world.is_valid_position(current_position, col_offset, row_offset):
-                    observation[center[0] + col_offset][center[1] + row_offset] = 1
+                    observation[center[0] +
+                                col_offset][center[1] + row_offset] = 1
                     # print("Position: ", (current_position[0] + col_offset, current_position[1] + row_offset), " is not valid")
                     continue
 
-                other_agent_index = self.world.current_position_is_of((current_col + col_offset, current_row + row_offset))
-                if other_agent_index != -1: # another agent is at this position
-                    observation[center[0] + col_offset][center[1] + row_offset] = 1
+                other_agent_index = self.world.current_position_is_of(
+                    (current_col + col_offset, current_row + row_offset))
+                if other_agent_index != -1:  # another agent is at this position
+                    observation[center[0] +
+                                col_offset][center[1] + row_offset] = 1
                     # print("Position: ", (current_position[0] + col_offset, current_position[1] + row_offset), " is occupied by another agent")
                     continue
 
                 # print("Position: ", (current_position[0] + col_offset, current_position[1] + row_offset), " is valid")
-        
+
         if self.is_first_observation:
             self.is_first_observation = False
-            self.observation_history = deque([observation for _ in range(3)], maxlen=3)
+            self.observation_history = deque(
+                [observation for _ in range(3)], maxlen=3)
         else:
             self.observation_history.append(observation)
 
         goal_col, goal_row = goal_position
         goal_distance = goal_col - current_col, goal_row - current_row
-            
+
         state = {
             "observation": torch.tensor(self.observation_history, dtype=torch.float32),
             "goal": torch.tensor(goal_distance, dtype=torch.float32),
@@ -397,7 +407,7 @@ class Agent:
         #     self.see_obstacle()
 
         return state
-        
+
     def get_epsilon(self):
         return_val = Agent.epsilon
         if Agent.epsilon > 0.1:
@@ -419,7 +429,7 @@ class Agent:
             move_index = torch.argmax(prediction).item()
 
         return move_index
-    
+
     def perform_action(self, action: int):
         if self.previous_move != action:
             self.num_direction_changes += 1
@@ -427,7 +437,7 @@ class Agent:
 
         self.distance_travelled += 1
         return self.world.perform_action(action, self.index_in_world)
-    
+
     # def play_step_force_policy(self, net: nn.Module, buffer: ReplayMemory):
     #     current_state = self.get_state()
     #     action = self.get_action(net, current_state)
@@ -439,7 +449,7 @@ class Agent:
     #     buffer.append(exp)
 
     #     return reward, done
-    
+
     @torch.no_grad()
     def play_step(self, net: nn.Module, buffer: ReplayMemory):
         current_state = self.get_state()
@@ -452,7 +462,7 @@ class Agent:
         buffer.append(exp)
 
         return reward, done
-        
+
 
 if __name__ == '__main__':
     world = World(5, 5, 3)
--- visualize.py
+++ visualize.py
@@ -1,14 +1,17 @@
 import pygame
-import math, random
+import math
+import random
 import time
 import numpy as np
 
+from models.QNet import QNetwork
 import torch
 import pytorch_lightning as pl
 
 from lightning_model import DQNLightning
 from config import Action, args
 from agent import World
+
 
 class Visualize:
     class Color():
@@ -25,15 +28,17 @@ class Visualize:
         self.box_width = math.floor(screen_width / num_columns)
         self.box_height = math.floor(screen_height / num_rows)
 
-        self.display = pygame.display.set_mode((self.screen_width, self.screen_height))
+        self.display = pygame.display.set_mode(
+            (self.screen_width, self.screen_height))
         self._init_display()
-        
+
         self.colors_list = [()] * num_boxes
-        self.colors_list = list(map(lambda _: tuple(np.random.randint(0, 256, size=3)), self.colors_list))
-    
+        self.colors_list = list(
+            map(lambda _: tuple(np.random.randint(0, 256, size=3)), self.colors_list))
+
     def _random_color(self,):
         return tuple(np.random.randint(0, 256, size=3))
-    
+
     def _init_display(self):
         pygame.display.get_surface().fill(Visualize.Color.WHITE)  # background
         self._draw_grid()
@@ -41,14 +46,20 @@ class Visualize:
     def _draw_grid(self):
         for x in range(0, self.screen_width, self.box_width):
             for y in range(0, self.screen_height, self.box_height):
-                pygame.draw.rect(self.display, Visualize.Color.BLACK, [x, y, self.box_width, self.box_height], 1)
-    
-    def _draw_box(self, x, y, color, isSmall = False): # x, y are indices in column and row
-        if not isSmall: pygame.draw.rect(self.display, color, [x * self.box_width, y * self.box_height, self.box_width, self.box_height])
-        else: pygame.draw.rect(self.display, color, [x * self.box_width + self.box_width / 4, y * self.box_height + self.box_height / 4, self.box_width / 2, self.box_height / 2])
+                pygame.draw.rect(self.display, Visualize.Color.BLACK, [
+                                 x, y, self.box_width, self.box_height], 1)
 
-    def _draw_goal(self, x, y, color): # x, y are indices in column and row
-        pygame.draw.ellipse(self.display, color, [x * self.box_width, y * self.box_height, self.box_width, self.box_height])
+    def _draw_box(self, x, y, color, isSmall=False):  # x, y are indices in column and row
+        if not isSmall:
+            pygame.draw.rect(self.display, color, [
+                             x * self.box_width, y * self.box_height, self.box_width, self.box_height])
+        else:
+            pygame.draw.rect(self.display, color, [x * self.box_width + self.box_width / 4, y *
+                             self.box_height + self.box_height / 4, self.box_width / 2, self.box_height / 2])
+
+    def _draw_goal(self, x, y, color):  # x, y are indices in column and row
+        pygame.draw.ellipse(self.display, color, [
+                            x * self.box_width, y * self.box_height, self.box_width, self.box_height])
 
     def _update_display(self):
         pygame.display.update()
@@ -56,18 +67,18 @@ class Visualize:
     def draw_state(self, world: World, old_position):
         for column in range(self.num_columns):
             for row in range(self.num_rows):
-                    self._draw_box(column, row, Visualize.Color.WHITE)
+                self._draw_box(column, row, Visualize.Color.WHITE)
 
         for agent_idx, agent_position in enumerate(old_position):
             color = self.colors_list[agent_idx]
             column, row = agent_position
             self._draw_box(column, row, color)
-        
+
         for agent_idx, agent_position in enumerate(world.current_positions):
             color = self.colors_list[agent_idx]
             column, row = agent_position
             self._draw_box(column, row, color, True)
-        
+
         for agent_idx, agent_goal in enumerate(world.goal_positions):
             color = self.colors_list[agent_idx]
             column, row = agent_goal
@@ -99,9 +110,17 @@ class Visualize:
         self._draw_goal(0, 0, (156, 100, 78))
         self._update_display()
 
+
 def main():
-    visualizer = Visualize(800, 800, args.num_rows, args.num_columns, args.num_agents)
-    model = DQNLightning.load_from_checkpoint(args.visualize_ckpt)
+    visualizer = Visualize(800, 800, args.num_rows,
+                           args.num_columns, args.num_agents)
+    # model = DQNLightning.load_from_checkpoint(args.visualize_ckpt)
+
+    model = QNetwork().to(torch.device("cpu"))
+    # model.load_model('runs/DDQN/run_1/model.pt')
+    model.load_state_dict(torch.load(
+        'runs/DDQN/run_1/model.pt', map_location=torch.device('cpu')))
+    # torch.load()
     model.eval()
 
     while True:
@@ -116,17 +135,18 @@ def main():
                     continue
                 agent = world.agent_lists[agent_idx]
                 state = agent.get_state()
-                state = torch.tensor(state)
+                # state = torch.tensor(state)
                 predictions = model(state)
 
-                sorted_values, sorted_actions = torch.sort(predictions, descending=True)
-                
+                sorted_values, sorted_actions = torch.sort(
+                    predictions, descending=True)
+
                 value_actions = list(zip(sorted_values, sorted_actions))
                 i, j = world.current_positions[agent_idx]
-                
+
                 tmp = 10
-                #print(frequent[i][j])
-                
+                # print(frequent[i][j])
+
                 for tup_idx in range(len(value_actions)):
                     val, action = value_actions[tup_idx]
                     if not world.is_valid_action(action, agent_idx):
@@ -141,22 +161,23 @@ def main():
                         val = val * (1 - tmp * frequent[i + 1][j] / 100.0)
                     elif action == Action.UP:
                         val = val * (1 - tmp * frequent[i][j - 1] / 100.0)
-                        
+
                     value_actions[tup_idx] = val, action
-                    
-                value_actions.sort(reverse=True, key = lambda x: x[0])
-                    
-                #print(value_actions[0][0], value_actions[1][0], value_actions[2][0], value_actions[3][0], value_actions[4][0])
-                #print(value_actions[0][1], value_actions[1][1], value_actions[2][1], value_actions[3][1], value_actions[4][1])
-                
+
+                value_actions.sort(reverse=True, key=lambda x: x[0])
+
+                # print(value_actions[0][0], value_actions[1][0], value_actions[2][0], value_actions[3][0], value_actions[4][0])
+                # print(value_actions[0][1], value_actions[1][1], value_actions[2][1], value_actions[3][1], value_actions[4][1])
+
                 sorted_actions = list(map(lambda x: x[1], value_actions))
-                
+
                 performed_the_move = False
                 for idx in range(len(sorted_actions)):
                     action = sorted_actions[idx]
-                                        
+
                     if world.is_valid_action(action, agent_idx):
-                        _, agent_done = world.perform_action(action, agent_idx)
+                        reward, agent_done, arrived = world.perform_action(
+                            action, agent_idx)
                         if agent_done:
                             agents_done[agent_idx] = True
                         performed_the_move = True
@@ -165,16 +186,16 @@ def main():
                 if not performed_the_move:
                     raise RuntimeError("Cannot perform any move!")
             visualizer.draw_state(world, old_position)
-            
+
             if all(agents_done):
                 pygame.time.wait(500)
                 old_position = world.current_positions
                 visualizer.draw_state(world, old_position)
-                pygame.time.wait(500)
+                pygame.time.wait(200)
                 done = True
                 break
             else:
-                pygame.time.wait(500)
+                pygame.time.wait(200)
 
 
 if __name__ == '__main__':
